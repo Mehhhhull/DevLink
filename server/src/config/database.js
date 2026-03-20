a77966661fa22b519be5ad92d797115f5067fa8a
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-03-20 20:57:43 +0530

    Server setup

---
+++ server/src/config/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import config from "./config.js"
+
+async function connectToDB(){
+
+  await mongoose.connect(config.MONGO_URI)
+
+  console.log("Connected to DB Reyy")
+}
+
+export default connectToDB;
