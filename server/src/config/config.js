6a5a8f6b7ecd75171157ff8e6139dc8f9e1bbb5f
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-03-20 21:08:13 +0530

    DB connected properly

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,6 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+if(!process.env.MONGO_URI){
+  throw new Error("MONGO_URI is not there in Environment Variables")
+}
+
 const config={
   MONGO_URI:process.env.MONGO_URI
 }
