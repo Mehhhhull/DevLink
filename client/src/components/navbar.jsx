fb892daab6714f11a12d40d0948363603c0d0aac
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-05-25 02:12:46 +0530

    Updated Landing Page Copy

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -5,24 +5,6 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const navlinks = [
-        {
-            href: "#creations",
-            text: "Team Match",
-        },
-        {
-            href: "#about",
-            text: "Idea Hub",
-        },
-        {
-            href: "#testimonials",
-            text: "Judge Score",
-        },
-        {
-            href: "#contact",
-            text: "Feature Request",
-        },
-    ];
     return (
         <>
             <motion.nav className="sticky top-0 z-50 flex items-center justify-between w-full h-18 px-6 md:px-16 lg:px-24 xl:px-32 backdrop-blur"
@@ -35,14 +17,6 @@ export default function Navbar() {
                     <img className="h-9 w-auto" src="/assets/logo.svg" width={138} height={36} alt="logo" />
                 </a>
 
-                <div className="hidden lg:flex items-center gap-8 transition duration-500">
-                    {navlinks.map((link) => (
-                        <Link key={link.href} to={link.href} className="hover:text-slate-300 transition">
-                            {link.text}
-                        </Link>
-                    ))}
-                </div>
-
                 <div className="hidden lg:block space-x-3">
                     <button className="px-6 py-2 bg-indigo-600 hover:bg-indigo-700 transition text-white rounded-md active:scale-95">
                         Try DevLink
@@ -51,20 +25,7 @@ export default function Navbar() {
                         Sign in
                     </button>
                 </div>
-                <button onClick={() => setIsMenuOpen(true)} className="lg:hidden active:scale-90 transition">
-                    <MenuIcon className="size-6.5" />
-                </button>
             </motion.nav>
-            <div className={`fixed inset-0 z-[100] bg-black/60 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 lg:hidden transition-transform duration-400 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                {navlinks.map((link) => (
-                    <Link key={link.href} to={link.href} onClick={() => setIsMenuOpen(false)}>
-                        {link.text}
-                    </Link>
-                ))}
-                <button onClick={() => setIsMenuOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-slate-100 hover:bg-slate-200 transition text-black rounded-md flex">
-                    <XIcon />
-                </button>
-            </div>
         </>
     );
 }
