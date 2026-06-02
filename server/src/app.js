331a4ffaf73c387cfd3d3ee93fa85d7dac8cecaa
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-06-02 23:51:38 +0530

    AI Judge Route

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
 import teamRouter from './routes/team.routes.js';
+import aiJudgeRouter from './routes/aiJudge.routes.js';
 
 const app=express();
 app.use(morgan("dev"))
@@ -12,5 +13,6 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRouter)
 app.use("/api/team",teamRouter)
+app.use("/api/ai-judge",aiJudgeRouter)
 
 export default app
