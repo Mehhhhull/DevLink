9fdddb7c49fb0ce0016796f88b7f0b860a8a4927
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-06-02 15:50:07 +0530

    Auth Middleware Added

---
+++ server/src/middlewares/auth.middlewares.js
@@ -0,0 +1,34 @@
+import userModel from '../models/user.model';
+import jwt from 'jsonwebtoken';
+
+
+async function authMiddleware(req, res, next) {
+  const token =
+      req.cookies.token ||
+      req.headers.authorization?.split(' ')[1]; // Extracting token from cookies or Authorization header
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized access,token is missing!!!',
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); //user id comes
+
+    const user = await userModel.findById(decoded.userId);
+    if(!user){
+      return res.status(401).json({
+      message:"User not found"
+    })
+  }
+
+    req.user = user;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Unauthorized access,token is Invalid!!!',
+    });
+  }
+}
